d5ba70e25b2bc516b7b4107e1407ae919ea63c28
chahyoil <chahyoil@gmail.com>
2024-09-28 15:46:28 +0900

    검색기능: 사용자입력 수집, URL 사용자 입력 동기화, 테이블 업데이트, 디바운싱

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,41 @@
-import React from "react";
+// /dashboard/invoices/page.tsx
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
 
-export default function InvoicesPage() {
-  return <div>InvoicesPage</div>;
+// 객체타입의 ?는 키가 없을수 있다는 의미
+export default async function InvoicesPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  // 객체?. 은 해당 객체가 없을 경우 에러 방지
+  // || 연산자는 왼쪽값이 있으면 왼쪽값 리턴하고 없으면 오른쪽값 리턴
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div>
+      <div>
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>
+        <Search placeholder='Search invoices...' />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className='mt-5 flex w-full justify-center'>
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,18 +1,41 @@
-'use client';
+// app/ui/search.tsx
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  // 검색 매개변수훅을 변수에 대입
+  const searchParams = useSearchParams();
+  // URL 경로 가져오기
+  const pathname = usePathname();
+  // 라우터 replace함수로 url 업데이트
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
+
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
+    <div className='relative flex flex-grow'>
+      <label htmlFor='search' className='sr-only'>
         Search
       </label>
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className='peer w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm placeholder:text-gray-500'
         placeholder={placeholder}
+        id='search'
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString()}
       />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <MagnifyingGlassIcon className='absolute left-3 top-1/2 h-[24px] w-[24px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
     </div>
   );
 }
