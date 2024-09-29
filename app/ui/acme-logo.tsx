e785c74a95e6cff2d0f57a450304e3b475d887fe
chahyoil <chahyoil@gmail.com>
2024-09-29 15:23:01 +0900

    로고 클릭시 메인으로 이동

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,16 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
+import Link from "next/link";
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <GlobeAltIcon className='h-12 w-12 rotate-[15deg]' />
+      <p className='text-[44px]'>
+        <Link href='/'>Acme</Link>
+      </p>
     </div>
   );
 }
