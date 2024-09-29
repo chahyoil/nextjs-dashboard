c3ec96ed9b85f5a60058176cfc4f06f73408dbfa
chahyoil <chahyoil@gmail.com>
2024-09-29 15:51:11 +0900

    메타데이터 추가

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,6 @@
 // /dashboard/invoices/page.tsx
+import { Metadata } from "next";
+
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
@@ -6,6 +8,10 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Invoices",
+};
 
 // 총페이지수 가져오는 함수
 import { fetchInvoicesPages } from "@/app/lib/data";
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
+// app/layout.tsx
+import { Metadata } from "next";
 import "@/app/ui/global.css";
 import { pretendard } from "@/app/ui/fonts";
+
+export const metadata: Metadata = {
+  // Acme Dashboard 기본 제목으로 사용, 서브페이지 제목있을 경우 템플릿사용, %s에 서브페이지 제목 들어옴
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard",
+  },
+  description: "차효일이 학원에서 들으면서 개발한 Next.js 코스 프로젝트",
+};
+
 export default function RootLayout({
   children,
 }: {
